25b51a6d275394b6c4f2aeab2e9e49a08b341902
Dennis Dang <dennisdang80@yahoo.com>
2023-10-28 21:35:17 -0500

    Completed half of cart page styling

--- front-end/src/pages/CartPage.tsx
+++ front-end/src/pages/CartPage.tsx
@@ -1,7 +1,8 @@
 import { useRecoilValue } from "recoil";
-import { Cart } from "../types/types";
+import { Cart, product } from "../types/types";
 import { cart } from "../atoms/cart";
 import CartTable from "../components/CartTable";
+import "../styles/CartPage.css";
 
 function CartPage() {
   const cartItems = useRecoilValue<Cart>(cart);
@@ -14,13 +15,63 @@ function CartPage() {
     "Total",
   ];
   return (
-    <body className="d-flex align-items-center py-4 bg-body-tertiary">
-      <div className="album py-5 bg-body-tertiary">
-        <div className="container">
-          <CartTable columns={columns} items={cartItems} />
+    <div className="container-fluid">
+      <div className="row">
+        <div className="col-md-4 summary-column">
+          <div className="summary-item-container p-2 py-4">
+            <div className="review-order-header">
+              Review Order ({cartItems.items.length})
+            </div>
+            <div className="subtotal-information">
+              <div className="col-md-6">
+                Subtotal:
+                <br></br>
+                Tax:
+              </div>
+              <div className="col-md-6">
+                ${(cartItems.total).toFixed(2)}
+                <br></br>
+                ${(cartItems.total *0.0625).toFixed(2)} 
+              </div>
+            </div>
+            <div className="total-information">
+              <div className="col-md-6">
+                Total:
+              </div>
+              <div className="col-md-6">
+              ${(parseFloat((cartItems.total *0.0625).toFixed(2)) + parseFloat((cartItems.total).toFixed(2))).toFixed(2)} 
+              </div>
+            </div>
+            <div className="button-container">
+              <button className="order-button">Place Order</button>
+              <button className="order-button">Cancel Order</button>
+            </div>
+          </div>
+        </div>
+        <div className="col-md-8">
+          {/* <CartTable columns={columns} items={cartItems} /> */}
+          {cartItems.items.map(
+          (
+            item: {
+              product: product;
+              toppings?: any;
+              notes?: string;
+            },
+            i: number
+          ) => (
+            <div className="drink-order-container">
+              Product Name: {item.product.name}
+              <br></br>
+              Product Category: {item.product.category}
+              <br></br>
+              Price: {item.product.price}
+              <br></br>
+            </div>
+          )
+        )}
         </div>
       </div>
-    </body>
+    </div>
   );
 }
 
