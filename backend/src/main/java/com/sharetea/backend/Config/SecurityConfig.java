30c377f1e24e539afd529e80253b4744019b95e7
edo41a <brownemgd@gmail.com>
2023-11-01 17:52:03 -0500

    fixed return format of some routes and modified config file

---
+++ backend/src/main/java/com/sharetea/backend/Config/SecurityConfig.java
@@ -0,0 +1,37 @@
+package com.sharetea.backend.Config;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+/**
+ * Configures our application with Spring Security to restrict access to our API endpoints.
+ */
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        /*
+        This is where we configure the security required for our endpoints and setup our app to serve as
+        an OAuth2 Resource Server, using JWT validation.
+        */
+        return http
+                .authorizeHttpRequests((authorize) -> authorize
+                    .requestMatchers(new AntPathRequestMatcher("/")).permitAll()
+                    .requestMatchers(new AntPathRequestMatcher("/product/**")).permitAll()
+                    .anyRequest().authenticated()
+                    //.requestMatchers("/api/private-scoped").hasAuthority("SCOPE_read:messages")
+                )
+                .cors(withDefaults())
+                .oauth2ResourceServer(oauth2 -> oauth2
+                    .jwt(withDefaults())
+                )
+                .build();
+    }
+}
