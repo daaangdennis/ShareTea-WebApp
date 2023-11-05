707265d8081dd0f3b3ca29e001d82b9955a73b36
Dennis Dang <dennisdang80@yahoo.com>
2023-11-04 20:50:06 -0500

    Adjusted spacing for order complete page

--- front-end/src/pages/CartPage.tsx
+++ front-end/src/pages/CartPage.tsx
@@ -12,7 +12,7 @@ function CartPage() {
   const [cartItems, setcartItems] = useRecoilState<Cart>(cart);
   const { user, isAuthenticated, isLoading } = useAuth0();
   const { loginWithRedirect } = useAuth0();
-  const [orderComplete, setOrderComplete] = useState<boolean>(false)
+  const [orderComplete, setOrderComplete] = useState<boolean>(true)
 
   const { getAccessTokenSilently } = useAuth0();
 
@@ -90,7 +90,7 @@ function CartPage() {
         <div className="col-md-8 mt-3 mt-md-0">
           {orderComplete ? 
             (
-              <div className="cartpage-order-complete">
+              <div className="cartpage-order-complete mt-lg-0 mt-4">
                 <svg xmlns="http://www.w3.org/2000/svg" width="100" height="100" fill="#15bd12" className="bi bi-check-circle" viewBox="0 0 16 16">
                   <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
                   <path d="M10.97 4.97a.235.235 0 0 0-.02.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-1.071-1.05z"/>
