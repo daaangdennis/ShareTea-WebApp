6aede84be6fd65be00250bd79a9063fccda38539
Enigmap <samtrythall@gmail.com>
2023-11-13 23:31:19 -0600

    Finished Product Usage and Restock Report

--- front-end/src/pages/StatsPage.tsx
+++ front-end/src/pages/StatsPage.tsx
@@ -1,65 +1,43 @@
 import "../styles/StatsPage.css";
-import {
-  XAxis,
-  YAxis,
-  CartesianGrid,
-  Tooltip,
-  AreaChart,
-  Area,
-} from "recharts";
+import { useRecoilValue } from "recoil";
+import { restockInventory } from "../atoms/statsItems";
+import React, { PureComponent, useEffect, useState } from "react";
+import { PieChart, Pie, Tooltip, Legend } from "recharts";
+import { useGetRestockReport } from "../apis/RestockReport";
+import ProductUsage from "../components/ProductUsage";
+import RestockInventory from "../components/RestockInventory";
+import ExcessItemsTable from "../components/ExcessItemsTable";
+
 function StatsPage() {
-  const data = [
-    {
-      name: "Page A",
-      uv: 4000,
-      pv: 2400,
-      amt: 2400,
-    },
-    {
-      name: "Page B",
-      uv: 3000,
-      pv: 1398,
-      amt: 2210,
-    },
-    {
-      name: "Page C",
-      uv: 2000,
-      pv: 9800,
-      amt: 2290,
-    },
-    {
-      name: "Page D",
-      uv: 2780,
-      pv: 3908,
-      amt: 2000,
-    },
-    {
-      name: "Page E",
-      uv: 1890,
-      pv: 4800,
-      amt: 2181,
-    },
-    {
-      name: "Page F",
-      uv: 2390,
-      pv: 3800,
-      amt: 2500,
-    },
-    {
-      name: "Page G",
-      uv: 3490,
-      pv: 4300,
-      amt: 2100,
-    },
-  ];
+  const [showProductUsage, setShowProductUsage] = useState(false);
+  const [showRestockUsage, setShowRestockUsage] = useState(false);
+  const [showExcessUsage, setShowExcessUsage] = useState(false);
+
+  const handleProductUsageButton = () => {
+    setShowProductUsage((prevValue) => !prevValue);
+  };
+
+  const handleRestockButton = () => {
+    setShowRestockUsage((prevValue) => !prevValue);
+  };
+
+  const handleExcessButton = () => {
+    setShowExcessUsage((prevValue) => !prevValue);
+  };
 
   return (
     <div className="StatsContainer">
       <div className="StatsButtonContainer">
         <button className="btn StatsButton">Sales Report</button>
-        <button className="btn StatsButton">Product Usage</button>
-        <button className="btn StatsButton">Restock Report</button>
-        <button className="btn StatsButton">Excess Report</button>
+        <button onClick={handleProductUsageButton} className="btn StatsButton">
+          Product Usage
+        </button>
+        <button onClick={handleRestockButton} className="btn StatsButton">
+          Restock Report
+        </button>
+        <button onClick={handleExcessButton} className="btn StatsButton">
+          Excess Report
+        </button>
         <button className="btn StatsButton">Popular Pairs</button>
       </div>
       <div className="GenerateButton">
@@ -72,41 +50,9 @@ function StatsPage() {
       </div>
 
       <div className="GraphContainer">
-        <AreaChart
-          width={730}
-          height={250}
-          data={data}
-          margin={{ top: 10, right: 30, left: 0, bottom: 0 }}
-        >
-          <defs>
-            <linearGradient id="colorUv" x1="0" y1="0" x2="0" y2="1">
-              <stop offset="5%" stopColor="#8884d8" stopOpacity={0.8} />
-              <stop offset="95%" stopColor="#8884d8" stopOpacity={0} />
-            </linearGradient>
-            <linearGradient id="colorPv" x1="0" y1="0" x2="0" y2="1">
-              <stop offset="5%" stopColor="#82ca9d" stopOpacity={0.8} />
-              <stop offset="95%" stopColor="#82ca9d" stopOpacity={0} />
-            </linearGradient>
-          </defs>
-          <XAxis dataKey="name" />
-          <YAxis />
-          <CartesianGrid strokeDasharray="3 3" />
-          <Tooltip />
-          <Area
-            type="monotone"
-            dataKey="uv"
-            stroke="#8884d8"
-            fillOpacity={1}
-            fill="url(#colorUv)"
-          />
-          <Area
-            type="monotone"
-            dataKey="pv"
-            stroke="#82ca9d"
-            fillOpacity={1}
-            fill="url(#colorPv)"
-          />
-        </AreaChart>
+        {showProductUsage && <ProductUsage />}
+        {showRestockUsage && <RestockInventory />}
+        {showExcessUsage && <ExcessItemsTable />}
       </div>
     </div>
   );
