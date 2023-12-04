77ab6369f6f3cfb1b712f173980818cdb657ffee
edo41a <brownemgd@gmail.com>
2023-12-04 13:35:59 -0600

    more docs

--- backend/src/main/java/com/sharetea/backend/Repositories/ItemToppingsRepository.java
+++ backend/src/main/java/com/sharetea/backend/Repositories/ItemToppingsRepository.java
@@ -10,10 +10,19 @@ import com.sharetea.backend.Entities.*;
 @Repository
 public interface ItemToppingsRepository extends JpaRepository<ItemToppings, Integer> {
 
-
+    /**
+     * Gets topping names associated with an order_product
+     * @param opID order_product_id to identify order_product
+     * @return List of strings with topping names
+     */
     @Query(value = "select name from item_toppings i join inventory inv on i.inventory_id = inv.inventory_id where order_product_id = ?1", nativeQuery = true)
     public List<String> getToppingsByopID(Integer opID);
 
+    /**
+     * Gets topping IDs associated with an order_product
+     * @param order_product_id order_product_id to identify order product
+     * @return List of integers with topping IDs
+     */
     @Query(value = "select item_toppings_id from item_toppings where order_product_id = ?1", nativeQuery = true)
     List<Integer> findByOrder_product_id(Integer order_product_id);
 
--- backend/src/main/java/com/sharetea/backend/Repositories/OrderProductRepository.java
+++ backend/src/main/java/com/sharetea/backend/Repositories/OrderProductRepository.java
@@ -10,12 +10,26 @@ import com.sharetea.backend.Entities.*;
 
 @Repository
 public interface OrderProductRepository extends JpaRepository<OrderProduct, Integer> {
+    /**
+     * Gets product_ids for 3 best selling products 
+     * @return List of integers with product_ids
+     */
     @Query(value = "select p.product_id from order_product op join product p on op.product_id = p.product_id join orders ord on op.order_id = ord.order_id where active = true group by p.product_id order by count(p.product_id) desc limit 3" , nativeQuery = true)
     public List<Integer> findBestSelling();
 
+    /**
+     * Gets products associated with an order
+     * @param orderID order_id to identify order
+     * @return JSON Object containing product details for an order
+     */
     @Query(value = "select order_product_id, product_id, note, sugar_level, ice_level from order_product where order_id = ?1" , nativeQuery = true)
     public List<Map<String,Object>> getProductsbyOrderID(Integer orderID);
 
+    /**
+     * Gets product ids associated with an order
+     * @param order_id order_id to identify order
+     * @return List of integers order_product_ids
+     */
     @Query(value = "select order_product_id from order_product where order_id = ?1", nativeQuery = true)
     List<Integer> findByOrder_id(Integer order_id);
 }
